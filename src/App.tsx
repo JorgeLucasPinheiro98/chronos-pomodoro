8796df911eb9791917ed13353f57586e80e45918
Jorge Lucas <Jorgejdn@hotmail.com>
2025-07-23 17:03:13 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { TimerIcon } from 'lucide-react';
 import '../src/styles/global.css';
 import '../src/styles/theme.css';
-import { Heading } from './components/Heading';
 
 export function App() {
   return (
-    <div>
-      <Heading>
-          <TimerIcon/>
-      </Heading>
-    </div>
+    <>
+      <div className='container'>
+        <div className='content'>
+          <section>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui minus,
+            laborum earum harum, facilis veniam eius quibusdam natus cupiditate
+            iste accusamus? Nobis, quas iure. Tempora iusto ipsum qui fugiat
+            officiis.
+          </section> 
+        </div>
+      </div>
+    </>
   );
 }
