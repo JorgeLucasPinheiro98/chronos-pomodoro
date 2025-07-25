e4dbd6d2eb5680d1faac078e462effa75d5fc702
Jorge Lucas <Jorgejdn@hotmail.com>
2025-07-25 16:19:06 -0300

    Adicionando a logica do app

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,50 @@
-import { HistoryIcon, HomeIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { HistoryIcon, HomeIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import style from './styles.module.css';
+import { useEffect, useState } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme = (localStorage.getItem('theme') as AvailableThemes) || 'dark'
+    return storageTheme
+  });
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+
+  const nextThemeIcon = {
+    dark: <SunIcon/>,
+    light: <MoonIcon/>
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme)
+  }, [theme]);
+
   return (
-  <nav className={style.menu}>
-    <a href="#" className={style.menuLink}>
-      <HomeIcon />
-    </a>
-    <a href="#" className={style.menuLink}>
-      <HistoryIcon />
-    </a>
-    <a href="#" className={style.menuLink}>
-      <SettingsIcon />
-    </a>
-    <a href="#" className={style.menuLink}>
-      <SunIcon />
-    </a>
-  </nav>
-)
+    <nav className={style.menu}>
+      <a href='#' className={style.menuLink}>
+        <HomeIcon />
+      </a>
+      <a href='#' className={style.menuLink}>
+        <HistoryIcon />
+      </a>
+      <a href='#' className={style.menuLink}>
+        <SettingsIcon />
+      </a>
+      <a href='#' className={style.menuLink} onClick={handleThemeChange}>
+        {nextThemeIcon[theme]}
+      </a>
+    </nav>
+  );
 }
